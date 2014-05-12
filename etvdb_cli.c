tg--/etvdb_cli

commit 47f7f3b4695c82a714cfdad10ead4f0f41920401

bugfix: fix absolute paths in templates

etvdb_cli.c
@@ -265,9 +265,13 @@ void modify_episode(Episode *e, const char *file, const char *template)
 		eina_strbuf_replace_all(strbuf, "/", "-");
 	}
 
-	path = ecore_file_dir_get(file);
-	eina_strbuf_prepend_char(strbuf, '/');
-	eina_strbuf_prepend(strbuf, path);
+	/* if we already have an absolute path starting with /, don't prepend another one */
+	buf = (char *)eina_strbuf_string_get(strbuf);
+	if (buf[0] != '/') {
+		path = ecore_file_dir_get(file);
+		eina_strbuf_prepend_char(strbuf, '/');
+		eina_strbuf_prepend(strbuf, path);
+	}
 
 	if (interactive) {
 		fprintf(stderr, "Rename \"%s\" to \"%s\"? \'y\' to accept: ", file, eina_strbuf_string_get(strbuf));

commit 3c5fe7362bf4864c5596aa5c467d8bfca6b81fa1

bugfix: rename in interactive mode works again
this bug has been introduced in 47f7f3b4695c82a714cfdad10ead4f0f41920401
important fix, so this includes a version bump

CMakeLists.txt
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 2.8)
 project(ETVDB_CLI)
-set(PACKAGE_VERSION 0.1.0)
+set(PACKAGE_VERSION 0.1.1)
 set(PACKAGE ${CMAKE_PROJECT_NAME})
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99")

etvdb_cli.c
@@ -274,8 +274,10 @@ Eina_Bool modify_episode(Episode *e, const char *file, const char *template)
 		eina_strbuf_prepend(strbuf, path);
 		free(path);
 	}
+	buf = NULL;
 
 	if (interactive) {
+		buf = malloc(32);
 		fprintf(stderr, "Rename \"%s\" to \"%s\"? \'y\' to accept: ", file, eina_strbuf_string_get(strbuf));
 		if (!fgets(buf, 32, stdin)) {
 			ERR("Invalid Input. Skipping rename.");
@@ -285,6 +287,7 @@ Eina_Bool modify_episode(Episode *e, const char *file, const char *template)
 			fprintf(stderr, "Skipping this file, per your wish.\n");
 			goto END;
 		}
+		free(buf);
 	}
 
 	if (!ecore_file_mv(file, eina_strbuf_string_get(strbuf))) {